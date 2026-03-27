ed4792b6e1ee37d1ea93e3a27b2c13586ede7760
Keya Chaudhary <keyabhamu@gmail.com>
2026-03-27 10:21:33 +0530

    Initialize Flask app for Churn Prediction Backend

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-# placeholder file for now
-print("Churn Prediction Backend is starting...")
-print("Experiment 6: Containerization Successful!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return {"status": "success", "message": "Experiment 6: Backend is LIVE"}
+
+if __name__ == "__main__":
+    # host='0.0.0.0' is REQUIRED for Docker to talk to your Windows host
+    app.run(host='0.0.0.0', port=8000)
